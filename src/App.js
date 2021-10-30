5ea94eeb9cfc5e64a5f55c5bcbb1215f5929f3fe
TuckTuckC <tuckermcraig@gmail.com>
2021-10-30 12:40:38 -0400

    added routs for more pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+
+// React Imports
 import {useState} from 'react';
-import Calculator from './Components/Calculator';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+// Component Imports
+import Calculator from './Components/Calculator/Calculator';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Budgets from './Components/Budgets/Budgets';
+import Profile from './Components/Profile/Profile';
+
+// FontAwesome Imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalculator } from '@fortawesome/free-solid-svg-icons'
 
@@ -15,17 +26,51 @@ function App() {
 	}
 
   	return (
-		<div className="App">
-			<div className='calcContainer'>
-				<Calculator calc = {calc} />
-				<FontAwesomeIcon 
-					icon={faCalculator} 
-					className='toggleCalc' 
-					onClick={() => toggle()}
-					style={iconPos === 1 ? { bottom:'37%' } : { bottom: '0.5vw' }}
-				/>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/about'>About</Link>
+						</li>
+						<li>
+							<Link to='/budgets'>Budgets</Link>
+						</li>
+						<li>
+							<Link to='/profile'>Profile</Link>
+						</li>
+					</ul>
+				</nav>
+				<Switch>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+					<Route exact path='/about'>
+						<About />
+					</Route>
+					<Route exact path='/budgets'>
+						<Budgets />
+					</Route>
+					<Route exact path='/profile'>
+						<Profile />
+					</Route>
+				</Switch>
 			</div>
-		</div>
+			<div className="App">
+				<div className='calcContainer'>
+					<Calculator calc = {calc} />
+					<FontAwesomeIcon 
+						icon={faCalculator} 
+						className='toggleCalc' 
+						onClick={() => toggle()}
+						style={iconPos === 1 ? { bottom:'37%' } : { bottom: '0.5vw' }}
+					/>
+				</div>
+			</div>
+		</Router>
   	);
 };
 
