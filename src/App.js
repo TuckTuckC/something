eb951c65c15bec2018f6d94bc08594deaea4878d
TuckTuckC <tuckermcraig@gmail.com>
2021-10-29 22:38:00 -0400

    toggleCalc button is now an icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import {useState} from 'react';
 import Calculator from './Components/Calculator';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { faCalculator } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 	
@@ -10,7 +12,11 @@ function App() {
   	return (
 		<div className="App">
 			<Calculator calc = {calc} />
-			<button onClick={() => setCalc(calc === 1 ? 0 : 1)}>Toggle Calc</button>
+			<FontAwesomeIcon 
+				icon={faCalculator} 
+				className='toggleCalc' 
+				onClick={() => setCalc(calc === 1 ? 0 : 1)}
+			/>
 		</div>
   	);
 };
--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -7,24 +7,25 @@ function Calculator(props) {
         return (
             <div className='calc'>
                 <div className='input'>Input</div>
+                <div className='output'>Output</div>
                 <div className='del'>del</div>
                 <div className='/'>/</div>
                 <div className='*'>*</div>
                 <div className='-'>-</div>
-                <div className='9'>9</div>
-                <div className='8'>8</div>
                 <div className='7'>7</div>
-                <div className='6'>6</div>
-                <div className='5'>5</div>
+                <div className='8'>8</div>
+                <div className='9'>9</div>
                 <div className='4'>4</div>
-                <div className='3'>3</div>
-                <div className='2'>2</div>
+                <div className='5'>5</div>
+                <div className='6'>6</div>
                 <div className='1'>1</div>
+                <div className='2'>2</div>
+                <div className='3'>3</div>
+                <div className='clr'>clr</div>
                 <div className='0'>0</div>
                 <div className='.'>.</div>
                 <div className='add'>+</div>
                 <div className='ent'>ent</div>
-                <div className='clr'>clr</div>
             </div>
             );
         } else {
